99ec9f052bca90c7e859b6cbe8be3d283609da96
Ed Ilyin <Ed.Il@live.com>
2022-02-06 18:58:11 +0200

    ig icons

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -8,19 +8,32 @@
 <div>
     <p>{emo}</p>
     <p>{name}</p>
-    <p><a href="https://www.instagram.com/{ig}">@{ig}</a></p>
-    <a href="tel:{phone}">{phone}</a>
+    <a href="https://www.instagram.com/{ig}">
+        <p>
+            <img src="./ig.png" alt="@" />
+            <span>{ig}</span>
+        </p>
+    </a>
+    <a href="tel:{phone}"><p>{phone}</p></a>
 </div>
 
 <style>
     div {
         background-color: #444d;
-        padding: 32px;
+        padding: 1rem;
         border-radius: 4px;
         box-shadow: 2px 2px 4px #222d;
     }
     a {
-        color: #bbdd;
+        color: #bbbb;
         text-decoration: none;
+    }
+    img {
+        max-width: 1rem;
+        max-height: 1rem;
+        vertical-align: middle;
+    }
+    span {
+        vertical-align: middle;
     }
 </style>
