bb2812d0ba060dadf3bcf6fc8e2d64a68ab4b41c
Ed Ilyin <Ed.Il@live.com>
2022-02-06 10:28:11 +0200

    adds bg video

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<style>
+#bgVideo {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    -webkit-transform: translateX(-50%) translateY(-50%);
+    transform: translateX(-50%) translateY(-50%);
+    min-width: 100%;
+    min-height: 100%;
+    width: auto;
+    height: auto;
+    z-index: -1000;
+    overflow: hidden;
+}
+</style>
+<video autoplay muted loop id="bgVideo">
+    <source src="bg.mp4" type="video/mp4">
+    <!-- <source src="assets/video/bg.webm" type="video/webm"> -->
+    Your browser does not support HTML5 video.
+</video>
+<h1>Sky Video</h1>
