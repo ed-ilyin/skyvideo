aae0d03aa79a3025261f11e2524828d66027ac70
Ed Ilyin <Ed.Il@live.com>
2022-02-06 18:03:36 +0200

    try overflow visible

--- src/lib/Background.svelte
+++ src/lib/Background.svelte
@@ -1,11 +1,11 @@
-<video autoplay muted loop playsInline id="bgVideo">
+<video autoplay muted loop playsInline>
     <source src="./bg.webm" type="video/webm" />
     <source src="./bg.mp4" type="video/mp4" />
     Your browser does not support HTML5 video.
 </video>
 
 <style>
-    #bgVideo {
+    video {
         position: fixed;
         top: 50%;
         left: 50%;
@@ -16,6 +16,6 @@
         width: auto;
         height: auto;
         z-index: -1000;
-        overflow: hidden;
+        overflow: visible;
     }
 </style>
